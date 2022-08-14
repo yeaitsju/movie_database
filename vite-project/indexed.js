121cad475fa889046afe29bbafde410ca6c1a591
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-08-13 23:20:24 -0400

    test

--- vite-project/indexed.js
+++ vite-project/indexed.js
@@ -1,9 +1,66 @@
 import { openDB } from 'idb';
+import'./movieStateDb'
 
-export const database = openDB('MovieStore', 1, {
-  upgrade(db) {
-    db.createObjectStore('favoriteMovies');
-    db.createObjectStore('favoriteMovies');
+
+
+
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+if (!indexedDB) {
+  console.log("IndexedDB could not be found in this browser.");
+}
+const request = indexedDB.open("MovieStoreDB", 1);
+
+request.onerror = function (event) {
+  console.error("An error occurred with IndexedDB");
+  console.error(event);
+};
+request.onupgradeneeded = function () {
+  //1
+  const db = request.result;
+
+  //2
+  const store = db.createObjectStore("MoviesToStore", { keyPath: "movies" }, { autoIncrement: true });
+// let movie = document.querySelector(movie-display)
+// console.log(movie)
+  //3
+  // store.createIndex("comment", ["comments"], { unique: false });
+
+  // 4
+
+};
+
+// // export const database = openDB('movieStore', 1, {
+  
+// //   upgrade(db) {
    
-  },
-});
+// //    const storeName = db.createObjectStore('moviesToStore');
+ 
+
+// //    const tx = db.transaction('keyval', 'readwrite');
+// //    const store = tx.objectStore('keyval');
+// //    const val = (await store.get('movie-display'));
+
+// // console.log(val)
+
+
+
+
+// //   //  store.put('state', 'key')
+   
+// //   //  const tx = db.transaction(moviesToStore, 'readwrite')
+// //   //  const dbstore = await tx.objectStore(moviesToStore) 
+
+// //   //   const val = 'this is movie'
+// //   //   const key = 'this is MOVIE'
+// //   //   const value = await dbstore.put(val, key)
+// //   //   await tx.done
+
+// //   },
+// // });
+export default 'indexed.js'
--- vite-project/movieStateDb.js
+++ vite-project/movieStateDb.js
@@ -1,4 +1,4 @@
-import { database } from "./indexed";
+import "./indexed";
 
 class Store {
   constructor(init) {
@@ -6,23 +6,24 @@ class Store {
     // store is context
     this.subscribers = [];
 
-    database.then(async (db) => {
-      this.db = db;
-      let movie = await db.get("movies", "movie");
+    // database.then(async (db) => {
+    //   this.db = db;
+    //   let movie = await db.get("moviesToStore", "movie");
 
-      if (movie) {
-        this.set("movie", movie);
-      }
-    });
+    //   if (movie) {
+    //     for(const [key,value] of Object.entries(movie))
+    //     this.set(key,value);
+    //   }
+    // });
 
     this.state = new Proxy(init, {
       async set(state, key, value) {
         state[key] = value;
         // w.e parent node innerHTML is null
         console.log(self.subscribers);
-        if (self.db) {
-          await self.db.put("movies", value, key);
-        }
+        // if (self.db) {
+        //   await self.db.put("moviesToStore", state.movieStore, "movie");
+        // }
         self.subscribers.forEach((subscriber) => subscriber(state));
 
         console.log("this is the set method");
@@ -42,10 +43,14 @@ class Store {
   }
   addMovie(state, value) {
     let newState = state.movies.push(value);
+    
     // returns length of array
     console.log(value);
     console.log(newState);
+    
+    
     console.log(this.state.movies);
+    c
     this.state = Object.assign(this.state, state);
 
     console.log(this.state);
@@ -53,22 +58,49 @@ class Store {
   getAllMovies() {
     return this.state.movies;
   }
-  favMovies(){
-    let favMovie = []
-    let favButton = document.getElementById("favbutton");
-    console.log(favButton)
+  addFavMovie(state, value) {
+    let newFavState = state.favMovies.push(value);
     
+    // returns length of array
+    console.log(value);
+    console.log(newFavState);
+    
+    
+    console.log(this.state.favMovies);
+   
+    this.state = Object.assign(this.state, state);
+
+    console.log(this.state);
   }
+  getAllFavMovies() {
+    return this.state.favMovies;
+  }
+  postMovie (context){
+    this.set("movieStore", context)
+  }
+
+
+//   favMovies(){
+//     let favMovie = []
+//     let favButton = document.getElementById("favbutton");
+//     console.log(favButton)
+    
+//   }
 }
 const store = new Store({ movies: [] });
+const favstore = new Store({ favMovies: [] });
+const commentStore = new Store({movieStore: 0})
 console.log(store);
+console.log(favstore)
 // create new store and want to sub to store
 // sub to store anytime state changes subscriber is going to call method (adding comment)
 store.subscribe((state) => {
   console.log(state);
   let movieState = state.movies;
-
+  let favMovieState = state.favMovies;
+  console.log(favMovieState)
   movieState.forEach((subMovies) => document.body.appendChild(subMovies));
+  favMovieState.forEach((subFavMovies) => document.body.appendChild(subFavMovies));
 });
 
 class Movie extends HTMLElement {
@@ -94,10 +126,11 @@ class Movie extends HTMLElement {
   connectedCallback() {
     // will be claaed fist time DOM is loaded
     this.innerHTML = `
-        <p> title:  ${this.title} </p>
-        <p> year: ${this.year} </p> 
-        <p> plot: ${this.plot} </p>
-        `;
+    <p> title:  ${this.title} </p>
+    <p> year: ${this.year} </p> 
+    <p> plot: ${this.plot} </p>
+    `
+
   }
 }
 // name.email,comment are properties of JS
@@ -109,7 +142,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector("#button");
   btn.addEventListener("click", (e) => {
     e.preventDefault();
-
+    
     let movieDisplay = document.createElement("movie-display");
     const title = document.querySelector("#title").value;
     console.log(title);
@@ -117,7 +150,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(year);
     const plot = document.querySelector("#plot").value;
     console.log(plot);
-
+    
     const apiKey = "896bcbd1";
     const url = `https://www.omdbapi.com/?t=${title}&y=${year}&plot=${plot}&apikey=${apiKey}`;
     console.log(url);
@@ -127,25 +160,117 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(data);
         console.log(data.Title);
         console.log(data.Poster);
-        const movieTemplate = `
+      //   const movieTemplate = `
       
-        <h2>${data.Title}</h2>
-        <p>${data.Year}</p>
-        <img src="${data.Poster}">
-        <p>${data.Plot}</p>
-        <button id="favbutton" type="submit">fave</button>
+      //   <h2>${data.Title}</h2>
+      //   <p>${data.Year}</p>
+      //   <img src="${data.Poster}">
+      //   <p>${data.Plot}</p>
        
-      `;
+       
+      // `;
 
-        let result = document.createTextNode(movieTemplate);
+        // let result = document.createTextNode(movieTemplate);
 
-        console.log(result);
-        document.body.appendChild(result);
+        // console.log(result);
+        // document.body.appendChild(result);
+      let dataTitle = data.Title;
+      let titleP = document.createTextNode(dataTitle)
+      console.log(titleP)
+          movieDisplay.appendChild(titleP)
+          let dataYear = data.Year;
+          let yearP = document.createTextNode(dataYear)
+          movieDisplay.appendChild(yearP)
+          let dataPlot = data.Plot;
+          let plotP = document.createTextNode(dataPlot)
+          movieDisplay.appendChild(plotP)
+          let dataPoster = document.createElement('img')
+          dataPoster.setAttribute("src", `${data.Poster}` )
+          movieDisplay.appendChild(dataPoster)
+          
+    
+          
+          
+          
+          
+          
+          
+          
+          let favLabel = document.createElement('label')
+          favLabel.setAttribute("for", "favs")
+          favLabel.setAttribute("id", "favslab")
+          favLabel.textContent = 'favorite this movie'
+          movieDisplay.appendChild(favLabel)
+          
+          let favsToggle = document.createElement("input")
+          favsToggle.setAttribute("type", "checkbox")
+          favsToggle.setAttribute("id", "favs")
+          movieDisplay.appendChild(favsToggle)
 
-        movieDisplay.setAttribute("title", title);
-        console.log(movieDisplay);
-        movieDisplay.setAttribute("year", data.Year);
-        movieDisplay.setAttribute("plot", data.Plot);
+
+          
+
+
+        favsToggle.addEventListener("change", (e) => {
+          if (e.target.checked){
+            console.log('its clicked bruh')
+            let favMovies = []; 
+            favMovies.push(movieDisplay)
+            console.log(favMovies)
+          let favMovieDisplay = document.createElement('div')
+          // let favTtitle = document.createElement('h1')
+          // favTitle.textContent = 'favorite movies'
+          // favMovieDisplay.appendChild(favTitle)
+          let favTitle = document.createTextNode(dataTitle)
+          console.log(favTitle)
+          favMovieDisplay.appendChild(favTitle)
+
+            let favYear = document.createTextNode(dataYear)
+            favMovieDisplay.appendChild(favYear)
+
+            let favPlot = document.createTextNode(dataPlot)
+            favMovieDisplay.appendChild(favPlot)
+
+            let favposter = document.createElement('img')
+            favposter.setAttribute("src", `${data.Poster}`)
+            favMovieDisplay.appendChild(favposter)
+
+         document.body.appendChild(favMovieDisplay)
+          console.log(favMovieDisplay)
+            favstore.addFavMovie(favstore.state, favMovieDisplay);
+              console.log(favstore.state)
+             
+          }else {
+            console.log('not checked bruh')
+          }
+        })
+        
+    
+     
+
+          
+        
+        let notes = document.createElement('textarea')
+        notes.setAttribute("rows", "5")
+        notes.setAttribute("cols", "20")
+        notes.setAttribute("id", "note")
+        movieDisplay.appendChild(notes)
+        let notesbtn = document.createElement("button")
+          notesbtn.textContent = 'Add notes'
+          movieDisplay.appendChild(notesbtn)
+          notesbtn.addEventListener("click", (e) => {
+           let notesVal = document.getElementById("note").value;
+           let res = document.createTextNode(notesVal)
+           let resdisplay = document.createElement('div')
+          resdisplay.appendChild(res)
+          movieDisplay.appendChild(resdisplay)
+         })
+
+
+
+
+
+
 
         document.body.appendChild(movieDisplay);
       });
@@ -154,3 +279,13 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 export default "./movieStateDb.js";
+
+
+
+
+
+// notes = display ?? blocked or none 
+
+//  to get it to show when the person clicked 
+
+// 
